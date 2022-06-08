5787f1d46de28013824d937ad2926dc607efd0d0
JHobson316 <86200876+JHobson316@users.noreply.github.com>
2022-06-07 21:51:44 -0500

    Add files via upload

---
+++ AboutMe.js
@@ -0,0 +1,130 @@
+"use strict"
+// Imma be honest, I need some way to relax so I'm making it video game questions.
+//function GuessingGame(){
+//    alert("Hello! It seems like you want to play a game.");
+//    let User = prompt("First off, what's your name?");
+//    alert("Alright, " + User + "! All you have to do to answer these questions is type 'y' if you think the statement is true or type 'n' if you thin that it's false. Let's get to the questions!");
+//    let Answer1 = prompt("Nintendo's flagship character, Mario, was initially named Jumpman.");
+//    let Answer1L = Answer1.toLowerCase(string);
+//    if (Answer1L == "y"){
+//        console.log("correct");
+//    }
+//    else if (Answer1L == "n"){
+//        console.log("incorrect");
+//    }
+//    let Answer2 = prompt("Sony bought Bungie Studios in 2022 for $4.8 Billion.");
+//    let Answer2L = Answer1.toLowerCase(string);
+//   if (Answer2L == "y"){
+//        console.log("incorrect");
+//    }
+//   else if (Answer2L == "n"){
+//        console.log("correct");
+//    }
+//    let Answer3 = prompt("Call of Duty: Modern Warfare 2 featured a controversial mission named 'No German.'");
+//   let Answer3L = Answer1.toLowerCase(string);
+//    if (Answer3L == "y"){
+//        console.log("incorrect");
+//    }
+//   else if (Answer3L == "n"){
+//        console.log("correct");
+//    }
+//    let Answer4 = prompt("In Bungie's Game, Destiny 2, a gun named 'Telesto' is known by the community for repeatedly breaking the game.");
+//    let Answer4L = Answer1.toLowerCase(string);
+//    if (Answer4L == "y"){
+//        console.log("correct");
+//    }
+//    else if (Answer4L == "n"){
+//        console.log("incorrect");
+//    }
+//    let Answer5 = prompt("I'm glad that I'm done with this.");
+//    let Answer5L = Answer1.toLowerCase(string);
+//    if (Answer5L == "y"){
+//        console.log("correct");
+//    }
+//    else if (Answer5L == "n"){
+//      console.log("incorrect");
+//    }
+//}
+// START HERE This function acts as an introduction once the user presses the button
+function GameIntro(){
+    alert("Hello! It seems like you want to play a game.");
+    //This prompt asks the user to input their name
+    let User = prompt("First off, what's your name?");
+    // Name is used in the next alert message
+    alert("Alright, " + User + "! All you have to do to answer these questions is type 'y' if you think the statement is true or type 'n' if you thin that it's false. Let's get to the questions!");
+    q1();
+}
+function q1(){
+    // The prompt asks for the user to input y or n. If the input matches one of these, 
+    let Answer1 = prompt("Nintendo's flagship character, Mario, was initially named Jumpman.");
+    let Answer1L = Answer1.toLowerCase();
+    if (Answer1L == "y"){
+        console.log("correct");
+        q2();
+    }
+    else if (Answer1L == "n"){
+        console.log("incorrect");
+        q2();
+    }
+    else {
+        q1();
+    }
+}
+function q2(){
+    let Answer2 = prompt("Sony bought Bungie Studios in 2022 for $4.8 Billion.");
+    let Answer2L = Answer2.toLowerCase();
+    if (Answer2L == "y"){
+        console.log("incorrect");
+        q3();
+    }
+    else if (Answer2L == "n"){
+        console.log("correct");
+        q3();
+    }
+    else {
+        q2();
+    }
+}
+function q3(){
+    let Answer3 = prompt("Call of Duty: Modern Warfare 2 featured a controversial mission named 'No German.'");
+    let Answer3L = Answer3.toLowerCase();
+    if (Answer3L == "y"){
+        console.log("incorrect");
+        q4();
+    }
+    else if (Answer3L == "n"){
+        console.log("correct");
+        q4();
+    }
+    else {
+        q3();
+    }
+}
+function q4(){
+    let Answer4 = prompt("In Bungie's Game, Destiny 2, a gun named 'Telesto' is known by the community for repeatedly breaking the game.");
+    let Answer4L = Answer4.toLowerCase();
+    if (Answer4L == "y"){
+        console.log("correct");
+        q5();
+    }
+    else if (Answer4L == "n"){
+        console.log("incorrect");
+        q5();
+    }
+    else {
+        q4();
+    }
+}
+function q5(){
+    let Answer5 = prompt("I'm glad that I'm done with this.");
+    let Answer5L = Answer5.toLowerCase();
+    if (Answer5L == "y"){
+        console.log("correct");
+    }
+    else if (Answer5L == "n"){
+        console.log("incorrect");
+    }
+    else {
+        q5();
+    }
+}
---
+++ class_02.md
@@ -0,0 +1,50 @@
+## HTML Answers
+
+- What is HTML in your own words??
+  - HTML is the structure of websites and
+- How do we start an HTML document?
+  - You start by declaring that the document is html.
+- List 5 formatting elements in HTML and their uses.
+  - Paragraph: Displays a block of text
+  - Head: Conatins info about the html document and what is used along with html
+  - Header: Top rectangle that includes navigation buttons
+  - Body: Everything under this tag is what is displayed to the end user
+  - Footer: Displays copyright info and other helpful information
+- Why do you use HTML in your own words?
+  - I use HTML in order to structure and display the content on websites.
+
+## CSS Answers
+
+- What does css stand for?
+  - Cascading Style Sheet
+- Why do we use CSS in your own words?
+  - We use css to style a website.
+- What is a property in CSS? What is the difference between a property and a selector?
+  - A property is a value on a selected element and a selector picks which element is being changed.
+- How can we position elements in CSS?
+  - We position elements based on how they come into contact with the user.
+
+## JS Answers
+
+- What is JavaScript in your own words?
+  - Javascript handles all of the operations and evaluations required on the website.
+- What are data types in Javascript? List 3 data types and their uses?
+  - The data types are string, which is used for words, integers, ,and floats, .
+- How do we add Javascript in our HTML document?
+  - We add javascript by using the "script" tag in our html and having the source be our js file.
+- What are 3 best practices in Javascript?
+  - Use shorthands, but carefully
+  - Naming functions names that briefly tell what the function does
+  - Sticking to a coding style
+
+## Markdown
+
+- Why do we use Markdown?
+  - We use markdown because it is very versatile with working with coding services and programs by placing things in plain text
+
+- How do you make a link in Markdown?
+    -
+
+- List 2 ways to make text **bold** in Markdown
+  - You can use **two asterisks** on either side of the text.
+  - You can also bold by _underlining_ the text.
